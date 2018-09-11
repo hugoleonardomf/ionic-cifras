d77592b6adf1f2cc1c0144bf47f70dd5582ae6bb
Hugo Leonardo <hugoleonardo@mbp-de-hugo.localdomain>
2018-09-10 22:16:45 -0300

    ...

--- src/pages/detalhes/detalhes.ts
+++ src/pages/detalhes/detalhes.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController, LoadingController, ModalController, Content } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController, ModalController, ActionSheetController, Content } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { ElementRef } from '@angular/core';
 
 @IonicPage()
 @Component({
@@ -11,6 +12,7 @@ import { Http } from '@angular/http';
 export class DetalhesPage {
 
   @ViewChild(Content) content: Content;
+  @ViewChild('divCifra') divCifra: ElementRef;
 
   public items: Array<any>;
   public cifraId: string;
@@ -21,7 +23,7 @@ export class DetalhesPage {
 
   private url: string = "http://www.sisvend.com.br/cifras/service/json.php?key=f1f58e8c06b2a61ce13e0c0aa9473a72&q=musicas&id=";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public loadingCtrl: LoadingController, public modalCtrl: ModalController, public http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, public loadingCtrl: LoadingController, public modalCtrl: ModalController, public http: Http, public actionSheetCtrl: ActionSheetController) {
     this.items = this.navParams.get('listaParam');
     this.cifraId = this.navParams.get('cifraIdParam');
     this.setPrevNextItem();
@@ -70,6 +72,73 @@ export class DetalhesPage {
         break;
       }
     }
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Escolha uma opção',
+      buttons: [
+        {
+          text: 'Texto muito pequeno',
+          handler: () => {
+            this.fontXSmall();
+          }
+        },
+        {
+          text: 'Texto pequeno',
+          handler: () => {
+            this.fontSmall();
+          }
+        },
+        {
+          text: 'Texto normal',
+          handler: () => {
+            this.fontNormal();
+          }
+        },
+        {
+          text: 'Texto grande',
+          handler: () => {
+            this.fontLarge();
+          }
+        },
+        {
+          text: 'Texto muito grande',
+          handler: () => {
+            this.fontXLarge();
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+ 
+    actionSheet.present();
+  }
+
+  fontXSmall(){
+    this.divCifra.nativeElement.style.fontSize = "x-small";
+  }
+
+  fontSmall(){
+    this.divCifra.nativeElement.style.fontSize = "small";
+  }
+
+  fontNormal(){
+    this.divCifra.nativeElement.style.fontSize = "14px";
+  }
+
+  fontLarge(){
+    this.divCifra.nativeElement.style.fontSize = "large";
+  }
+
+  fontXLarge(){
+    this.divCifra.nativeElement.style.fontSize = "x-large";
   }
 
   //scroll
